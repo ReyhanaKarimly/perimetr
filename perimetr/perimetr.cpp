5ce9adab6a7c69bf3e3f569523c9ba21c1539c99
ReyhanaKarimly <karimlyreyhana@gmail.com>
2017-03-01 00:03:55 +0400

    ppp

---
+++ perimetr/perimetr.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int a,b,c,d;
+	scanf("%d%d",&a,&b);
+		c=2*(a+b);
+	d=a*b;
+	printf("perimetr=%d",c);
+printf("\narea=%d",d);
+getch();
+}
+	 
